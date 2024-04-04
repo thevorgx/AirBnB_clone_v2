efde1b772dd9d8206e857ff470548ba4851925f2
thevorgx <kh.elallali@gmail.com>
2024-04-04 07:38:34 +0000

    task 0 not complete (missing configuration for Nginx for url with /hbnb_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# script to setup web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared
+
+echo "Hello Vorg" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo service nginx restart
