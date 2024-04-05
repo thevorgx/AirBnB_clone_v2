c552d40024007a0c75277b3af86929e2ea8560cf
thevorgx <kh.elallali@gmail.com>
2024-04-05 02:33:06 +0000

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,15 +3,32 @@
 
 from fabric.api import *
 from os.path import exists
+from os import path, makedirs
+from datetime import datetime
 
 
 env.user = 'ubuntu'
 env.hosts = ['18.206.198.128', '107.23.109.180']
 
 
+def do_pack():
+    """generate a .tgz archive from the contents of the web_static folder"""
+    if not path.exists("versions"):
+        makedirs("versions")
+
+    creation_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    tgz_archive_name = "web_static_{}.tgz".format(creation_time)
+    tgz_archive_path = path.join("versions", tgz_archive_name)
+    make_tgz = local("tar -cvzf {} web_static".format(tgz_archive_path))
+
+    if make_tgz.succeeded:
+        return (tgz_archive_path)
+    else:
+        return (None)
+
+
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
+    """distributes an archive to my web servers"""
     if exists(archive_path) is False:
         return False
 
