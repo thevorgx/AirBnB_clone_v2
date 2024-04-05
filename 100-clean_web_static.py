32b719f0ada349ee87526047d8aa575c64e83c93
thevorgx <kh.elallali@gmail.com>
2024-04-05 02:45:54 +0000

    place holders

---
+++ 100-clean_web_static.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""place holder for tomorrow"""
+
+
+def do_clean(number=0):
+    """place holder for tomorrow"""
+    pass
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""place holder for tomorrow"""
+
+
+def deploy():
+    """place holder for tomorrow"""
+    pass
