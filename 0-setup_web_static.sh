f45c5af4bae22e65daa52b6282039893e8629165
thevorgx <kh.elallali@gmail.com>
2024-04-08 01:30:24 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,10 +6,9 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "Hello Vorg" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 sudo service nginx restart
