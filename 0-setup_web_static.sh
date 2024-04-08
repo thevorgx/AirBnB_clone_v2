e530e9de355a6f8fda8be5e5c9dcee5978655450
thevorgx <kh.elallali@gmail.com>
2024-04-08 01:26:48 +0000

    update 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,5 @@ echo "Hello Vorg" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 sudo service nginx restart
